7368977e686a31a9d2584139c1bf0597ad358d6e
Wayan Wiprayoga <iwawiwi@gmail.com>
2013-09-20 16:50:03 +0700

    + comment on GD to understand the program

--- experiment_1.py
+++ experiment_1.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from time import time
 
@@ -8,134 +7,161 @@ y_true = np.sin(x)
 
 # noisy signal
 n_sample = 10
-x_sample = np.linspace(0.0, 2*np.pi, n_sample, True)
+x_sample = np.linspace(0.0, 2 * np.pi, n_sample, True)
 noise = np.random.normal(0, 0.15, len(x_sample))
 y_sample = np.sin(x_sample) + noise
 
 import grad_descent as reg
+
+# Orde
 order = 3
-n_gd_iter = 150
+# Number of Iteration
+n_gd_iter = 1500
+# Learning rate
 gd_alpha = 0.0001
+# Stocasthic GD sample to take
 sgd2_sample_size = 3
+
+# Standard Regression
 t0 = time()
 w_stand, J_stand = reg.standReg(x_sample, y_sample, order)
+# Gradient Descent
 t1 = time()
 w_gd, J_gd_history = reg.gradDescent(x_sample, y_sample, order, n_gd_iter, gd_alpha)
 print 'GD finished'
+# Stochastic Gradient Descent
 t2 = time()
 w_sgd, J_sgd_history = reg.stocGradDescent(x_sample, y_sample, order, n_gd_iter, gd_alpha)
 print 'SGD finished'
+# Stochastic Gradient Descent 2
 t3 = time()
 w_sgd2, J_sgd2_history = reg.stocGradDescent2(x_sample, y_sample, order, n_gd_iter, gd_alpha, sgd2_sample_size)
 print 'SGD2 finished'
 t4 = time()
+# Gradient Descent with random W initialized
+w_gdrw, J_gdRndW_history = reg.gradDescentWRand(x_sample, y_sample, order, n_gd_iter, gd_alpha)
+print 'GD with Rand. W finished'
+t5 = time()
+
+# Compute time elapsed for each regression
 ct_normal_eq = t1 - t0
 ct_grad_desc = t2 - t1
 ct_stoc_grad_desc = t3 - t2
 ct_stoc_grad_desc2 = t4 - t3
+ct_gdRndW = t5 - t4
 
 # create model for drawing
 y_model_stand = reg.createModel(x, w_stand)
 y_model_gd = reg.createModel(x, w_gd)
 y_model_sgd = reg.createModel(x, w_sgd)
 y_model_sgd2 = reg.createModel(x, w_sgd2)
+y_model_gdrw = reg.createModel(x, w_gdrw)
 
 # plot
 import matplotlib.pyplot as plt
 
 # plot results
-fig = plt.figure()
+plt_title = '[N' + str(n_sample) + '][M' + str(order) + '][alpha' + \
+            str(gd_alpha) + '][SGD' + str(sgd2_sample_size) + '][iter' + \
+            str(n_gd_iter) + ']'
+fig = plt.figure(plt_title)
 ax = fig.add_subplot(111)
 ax.plot(x, y_true, 'g-', linewidth=2, label='True')
 ax.scatter(x_sample, y_sample, s=50, facecolors='none', edgecolors='b', linewidths=0.5, label='Data')
-ax.plot(x, y_model_stand, 'r--', linewidth=2, label='Standard')
-ax.plot(x, y_model_gd, 'g--', linewidth=2, label='GD')
-ax.plot(x, y_model_sgd, 'm--', linewidth=2, label='SGD')
-ax.plot(x, y_model_sgd2, 'c--', linewidth=2, label='SGD2')
+ax.plot(x, y_model_stand, 'r--', linewidth=2, label='Standard')  # plot LR Standard
+ax.plot(x, y_model_gd, 'g--', linewidth=2, label='GD')  # Plot GD
+ax.plot(x, y_model_sgd, 'm--', linewidth=2, label='SGD')  # Plot SGD
+ax.plot(x, y_model_sgd2, 'c--', linewidth=2, label='SGD2')  # Plot SGD2
+ax.plot(x, y_model_gdrw, 'y--', linewidth=2, label='GDRW')  # Plot GD RW
 plt.xlabel('X')
 plt.ylabel('y')
 plt.title('Regression with M = ' + str(order) + ', N = ' + str(len(x_sample)))
 plt.legend()
 plt.grid()
 # plt.show()
-
-# plot GD error profile
-n_iter = len(J_gd_history)
-x_error = np.arange(n_iter)
-J_error = [J_stand] * n_iter
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(x_error, J_error, 'r--', linewidth=2, label='Stand')
-ax.plot(x_error, J_gd_history, 'g--', linewidth=2, label='GD')
-plt.xlabel('Error Profile')
-plt.ylabel('Iteration')
-plt.title('GD Error Profile with M = ' + str(order) + ', N = ' + str(len(x_sample)))
-plt.legend()
-plt.grid()
-# plt.show()
-
-# plot SGD error profile
-n_iter = len(J_sgd_history)
-x_error = np.arange(n_iter)
-J_error = [J_stand] * n_iter
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(x_error, J_error, 'r--', linewidth=2, label='Stand')
-ax.plot(x_error, J_sgd_history, 'g--', linewidth=2, label='SGD')
-plt.xlabel('Error Profile')
-plt.ylabel('Iteration')
-plt.title('SGD Error Profile with M = ' + str(order) + ', N = ' + str(len(x_sample)))
-plt.legend()
-plt.grid()
-# plt.show()
-
-# plot SGD2 error profile
-n_iter = len(J_sgd2_history)
-x_error = np.arange(n_iter)
-J_error = [J_stand] * n_iter
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(x_error, J_error, 'r--', linewidth=2, label='Stand')
-ax.plot(x_error, J_sgd2_history, 'g--', linewidth=2, label='SGD2')
-plt.xlabel('Error Profile')
-plt.ylabel('Iteration')
-plt.title('SGD2 Error Profile with M = ' + str(order) + ', N = ' + str(len(x_sample)))
-plt.legend()
-plt.grid()
-# plt.show()
-
-# plot computation time comparison
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cts = [np.log(ct_normal_eq), np.log(ct_grad_desc), np.log(ct_stoc_grad_desc), np.log(ct_stoc_grad_desc2)]
-b = [0.15, 0.35, 0.55, 0.75]
-plt.xlim(0.0, 1.0)
-tick_offset = [0.05] * 4
-xticks = [x + y for x, y in zip(b, tick_offset)]
-ax.set_xticks(xticks)
-ax.set_xticklabels(('NE', 'GD', 'SGD', 'SGD2'))
-ax.bar(b, cts, width=0.1, color='r')
-ax.set_yscale('symlog', linthreshy=1)
-plt.xlabel('Methods')
-plt.ylabel('Time (s)')
-plt.title('Computation Time of NE,GD,SGD,SGD2 with Iter = ' + str(n_iter))
-plt.grid()
-# plt.show()
-
-
+#
+# # plot GD error profile
+# n_iter = len(J_gd_history)
+# x_error = np.arange(n_iter)
+# J_error = [J_stand] * n_iter
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.plot(x_error, J_error, 'r--', linewidth=2, label='Stand')
+# ax.plot(x_error, J_gd_history, 'g--', linewidth=2, label='GD')
+# plt.xlabel('Error Profile')
+# plt.ylabel('Iteration')
+# plt.title('GD Error Profile with M = ' + str(order) + ', N = ' + str(len(x_sample)))
+# plt.legend()
+# plt.grid()
+# # plt.show()
+#
+# # plot SGD error profile
+# n_iter = len(J_sgd_history)
+# x_error = np.arange(n_iter)
+# J_error = [J_stand] * n_iter
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.plot(x_error, J_error, 'r--', linewidth=2, label='Stand')
+# ax.plot(x_error, J_sgd_history, 'g--', linewidth=2, label='SGD')
+# plt.xlabel('Error Profile')
+# plt.ylabel('Iteration')
+# plt.title('SGD Error Profile with M = ' + str(order) + ', N = ' + str(len(x_sample)))
+# plt.legend()
+# plt.grid()
+# # plt.show()
+#
+# # plot SGD2 error profile
+# n_iter = len(J_sgd2_history)
+# x_error = np.arange(n_iter)
+# J_error = [J_stand] * n_iter
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.plot(x_error, J_error, 'r--', linewidth=2, label='Stand')
+# ax.plot(x_error, J_sgd2_history, 'g--', linewidth=2, label='SGD2')
+# plt.xlabel('Error Profile')
+# plt.ylabel('Iteration')
+# plt.title('SGD2 Error Profile with M = ' + str(order) + ', N = ' + str(len(x_sample)))
+# plt.legend()
+# plt.grid()
+# # plt.show()
+#
+# # plot computation time comparison
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# cts = [np.log(ct_normal_eq), np.log(ct_grad_desc), np.log(ct_stoc_grad_desc), np.log(ct_stoc_grad_desc2)]
+# b = [0.15, 0.35, 0.55, 0.75]
+# plt.xlim(0.0, 1.0)
+# tick_offset = [0.05] * 4
+# xticks = [x + y for x, y in zip(b, tick_offset)]
+# ax.set_xticks(xticks)
+# ax.set_xticklabels(('NE', 'GD', 'SGD', 'SGD2'))
+# ax.bar(b, cts, width=0.1, color='r')
+# ax.set_yscale('symlog', linthreshy=1)
+# plt.xlabel('Methods')
+# plt.ylabel('Time (s)')
+# plt.title('Computation Time of NE,GD,SGD,SGD2 with Iter = ' + str(n_iter))
+# plt.grid()
+# # plt.show()
+#
+#
 # plot computation time comparison
 fig = plt.figure()
 ax = fig.add_subplot(111)
 cts = [ct_normal_eq, ct_grad_desc, ct_stoc_grad_desc, ct_stoc_grad_desc2]
 b = [0.15, 0.35, 0.55, 0.75]
+# cts = [ct_normal_eq, ct_grad_desc, ct_gdRndW, ct_stoc_grad_desc, ct_stoc_grad_desc2]
+# b = [0.15, 0.30, 0.45, 0.60, 0.75]
 plt.xlim(0.0, 1.0)
 tick_offset = [0.05] * 4
 xticks = [x + y for x, y in zip(b, tick_offset)]
 ax.set_xticks(xticks)
 ax.set_xticklabels(('NE', 'GD', 'SGD', 'SGD2'))
+# ax.set_xticklabels(('NE', 'GD', 'GDRW', 'SGD', 'SGD2'))
 ax.bar(b, cts, width=0.1, color='r')
 plt.xlabel('Methods')
 plt.ylabel('Time (s)')
-plt.title('Computation Time of NE,GD,SGD,SGD2 with Iter = ' + str(n_iter))
+plt.title('Computation Time of NE,GD,SGD,SGD2 with Iter = ' + str(n_gd_iter))
+# plt.title('Computation Time of NE,GD,SGD,SGD2 with Iter = ' + str(n_iter))
+# plt.title('Computation Time of NE,GD,SGD,SGD2,GDRW with Iter = ' + str(n_gd_iter))
 plt.grid()
 plt.show()
--- grad_descent.py
+++ grad_descent.py
@@ -1,8 +1,8 @@
-
 import numpy as np
 import random as rnd
 
 
+# Create matrix X
 def createX(x, m):
     X = np.zeros(shape=((len(x), m + 1)))
     row_index = 0
@@ -12,20 +12,23 @@ def createX(x, m):
             xrow.append(pow(x_elem, i))
         X[row_index:] = xrow
         row_index += 1
-    # print 'rank: ', np.rank(np.mat(X))
+        # print 'rank: ', np.rank(np.mat(X))
     return np.mat(X)  # return matrix instead of ndarray
 
 
+# Compute RMSE
 def computeCost(X, y, w):
-    n = y.size
-    predictions = X * w
-    err = predictions - y
+    n = y.size # Number of sample
+    predictions = X * w # Predicition of y given current w
+    err = predictions - y # error prediction
+    # RMSE
     sqErr = np.power(err, 2)
     sqErrSum = sqErr.sum()
-    J = (1.0 / (2*n)) * sqErrSum
+    J = (1.0 / (2 * n)) * sqErrSum
     return J
 
 
+# Standard Regression
 def standReg(x, y, m):
     X = createX(x, m)  # create X
     y = np.mat(y)
@@ -40,68 +43,139 @@ def standReg(x, y, m):
     return w, J
 
 
+# Gradient Descent Regression
 def gradDescent(x, y, m, num_iters=1500, alpha=0.0001):
-    n = y.size
+    """
+    Regression using Gradient Descent
+
+    x = X point's sample
+    y = Y point's sample
+    m = Order
+    num_iters = number of iteration, default 1500
+    alpha = learning rate alpha, default 0.0001
+    """
+    n = y.size # Number of sample, also the same with do x.size
     X = createX(x, m)  # create X
-    y = np.mat(y)
+    y = np.mat(y) # convert y to matrix data type
     y = y.T  # prepare y
-    w = np.mat(np.zeros(shape=(m+1, 1)))
-    E_history = np.zeros(shape=(num_iters, 1))
-    prevE = float("inf")
-    for i in range(num_iters):
-        E = computeCost(X, y, w)
-        if E < prevE:
-            predictions = X * w
-            diff = predictions - y
-            for j in range(m+1):
-                error = diff.T * X[:, j]
-                w[j][0] = w[j][0] - alpha * (1.0/n) * error.sum()
-            E_history[i, 0] = E  # computeCost(X, y, w)
-            prevE = E
-        else:
+    w = np.mat(np.zeros(shape=(m + 1, 1))) # Prepare w with ALL ZERO value
+    E_history = np.zeros(shape=(num_iters, 1)) # Store error history for each iteration
+    prevE = float("inf") # assign initial previous error as Infinite (very large error)
+    for i in range(num_iters): # Loop for 'num_iters' iterations
+        E = computeCost(X, y, w) # compute current error of predicition given w
+        if E < prevE: # Make sure current error is convergent (always smaller)
+            # Some of following expression can be optimized because in computeCost() we have compute some of the value
+            predictions = X * w # calculate current prediction
+            diff = predictions - y # error of prediction
+            for j in range(m + 1): # iteration to update all of the w values
+                error = diff.T * X[:, j] # compute prediction error of each order
+                # w_i' = w_i - alpha * (sum of regression errors of orde i-th) * X_i
+                w[j][0] = w[j][0] - alpha * (1.0 / n) * error.sum() # update w, TODO: why 1/n * sum of errors?
+            E_history[i, 0] = E  # computeCost(X, y, w), store error history
+            prevE = E # update previous error as current error
+        else: # If the error increased, halt process and throws error
             print 'diverged! try using smaller alpha'
+            break # break loop
     return w, E_history
 
+
+# Stochastic Gradient Descent using 1 random sample
 def stocGradDescent(x, y, m, num_iters=1500, alpha=0.0001):
+    """
+    Regression using Stochastic Gradient Descent with one random sample choosen
+
+    x = X point's sample
+    y = Y point's sample
+    num_iters = number of iterations, default 1500
+    alpha = learning rate alpha, default 0.0001
+    """
     n = y.size
     X = createX(x, m)  # create X
     y = np.mat(y)
     y = y.T  # prepare y
-    w = np.mat(np.zeros(shape=(m+1, 1)))
+    w = np.mat(np.zeros(shape=(m + 1, 1))) # initialize w with all ZERO value
     E_history = np.zeros(shape=(num_iters, 1))
     for i in range(num_iters):
-        dataIndex = rnd.randint(0, n-1)
-        E = computeCost(X[dataIndex], y[dataIndex], w)
-        predictions = X[dataIndex] * w
-        diff = predictions - y[dataIndex]
-        for j in range(m+1):
-            error = diff.T * X[dataIndex, j]
-            w[j][0] = w[j][0] - alpha * (1.0/n) * error
-
+        dataIndex = rnd.randint(0, n - 1) # choose ONE random index
+        E = computeCost(X[dataIndex], y[dataIndex], w) # compute prediction error using the choosen index
+        # Some of following expression can be optimized because in computeCost() we have compute some of the value
+        predictions = X[dataIndex] * w # the prediction value of prediction
+        diff = predictions - y[dataIndex] # prediction error
+        for j in range(m + 1): # update all of w values
+            error = diff.T * X[dataIndex, j] # compute prediction error of each order using choosen sample
+            w[j][0] = w[j][0] - alpha * (1.0 / n) * error
+        # we can't MAINTAIN THE ERROR to be CONVERGENT, because we ONLY look at SOME data sample, NOT ALL the data!
         E_history[i, 0] = E  # computeCost(X, y, w)
     return w, E_history
 
 
+# Stochastic Gradient Descent using more than one sample
 def stocGradDescent2(x, y, m, num_iters=1500, alpha=0.0001, sample_size=4):
+    """
+    Regression using Stochastic Gradient Descent with more than one random sample choosen
+
+    x = X point's sample
+    y = Y point's sample
+    num_iters = number of iterations, default 1500
+    alpha = learning rate alpha, default 0.0001
+    sample_size = random sample choosen, default 4 random sample
+    """
     n = y.size
     X = createX(x, m)  # create X
     y = np.mat(y)
     y = y.T  # prepare y
-    w = np.mat(np.zeros(shape=(m+1, 1)))
+    w = np.mat(np.zeros(shape=(m + 1, 1)))
     E_history = np.zeros(shape=(num_iters, 1))
     for i in range(num_iters):
         randIndex = rnd.sample(range(len(X)), sample_size)
         E = computeCost(X[randIndex], y[randIndex], w)
         predictions = X[randIndex] * w
         diff = predictions - y[randIndex]
-        for j in range(m+1):
+        for j in range(m + 1):
             error = diff.T * X[randIndex, j]
-            w[j][0] = w[j][0] - alpha * (1.0/n) * error.sum()
+            w[j][0] = w[j][0] - alpha * (1.0 / n) * error.sum()
 
         E_history[i, 0] = E  # computeCost(X, y, w)
     return w, E_history
 
 
+# TODO: EXPERIMENTS ONLY
+def gradDescentWRand(x, y, m, num_iters=1500, alpha=0.0001):
+    n = y.size # Number of sample, also the same with do x.size
+    X = createX(x, m)  # create X
+    y = np.mat(y) # convert y to matrix data type
+    y = y.T  # prepare y
+    # randW = np.mat(np.random.normal(size=m+1))
+    # w = randW.T
+    w = np.mat(np.zeros(shape=(m + 1, 1))) # Prepare w with ALL ZERO value
+
+    # randomize intial w
+    randW = rnd.randint(0, n - 1) / n
+    for i in range(m+1):
+        if i == 0:
+            continue
+        w[i] = np.power(randW, i)
+
+    E_history = np.zeros(shape=(num_iters, 1)) # Store error history for each iteration
+    prevE = float("inf") # assign initial previous error as Infinite (very large error)
+    for i in range(num_iters): # Loop for 'num_iters' iterations
+        E = computeCost(X, y, w) # compute current error of predicition given w
+        if E < prevE: # Make sure current error is convergent (always smaller)
+            # Some of following expression can be optimized because in computeCost() we have compute some of the value
+            predictions = X * w # calculate current prediction
+            diff = predictions - y # error of prediction
+            for j in range(m + 1): # iteration to update all of the w values
+                error = diff.T * X[:, j] # compute prediction error of each order
+                # w_i' = w_i - alpha * (sum of regression errors of orde i-th) * X_i
+                w[j][0] = w[j][0] - alpha * (1.0 / n) * error.sum() # update w, TODO: why 1/n * sum of errors?
+            E_history[i, 0] = E  # computeCost(X, y, w), store error history
+            prevE = E # update previous error as current error
+        else: # If the error increased, halt process and throws error
+            print 'diverged! try using smaller alpha'
+            break # break loop
+    return w, E_history
+
+# Create Model
 def createModel(x, w):
     y = np.zeros(len(x))
     y = np.mat(y)
